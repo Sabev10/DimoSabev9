cd9ab9987055fa02b59179e462d8e33943f1620c
Dimitargit <>
2023-01-06 11:44:04 +0200

    15

---
+++ project/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] mass = new int[n+1];
+
+            int min = mass[0];
+
+            for (int i = 0; i < n; i++) {
+                if (mass[i] < min) {
+                    mass[i] = min;
+                }
+            }
+            Console.WriteLine(min);
+
+            
+        }
+    }
+}
